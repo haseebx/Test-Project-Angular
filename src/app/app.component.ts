eb193ce5ca1137e19ce9361c43eb9d2247c8b600
Haseebx <hasbm125@gmail.com>
2023-08-08 16:45:12 +0500

    form data in object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -28,7 +29,10 @@ export class AppComponent {
       console.log('in If');
       return;
     }
-
     this.state = true;
+  }
+  getData(val:NgForm){
+    console.log(val);
+    
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-
-
 import {UserAuthModule} from "./user-auth/user-auth.module";
 import { HeaderComponent } from './header/header.component'
-
+import {FormsModule} from "@angular/forms"
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +14,7 @@ import { HeaderComponent } from './header/header.component'
   imports: [
     BrowserModule,
     UserAuthModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
